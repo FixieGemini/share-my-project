a1d2183b68c29fddfe598ceb342229e26b373e0f
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2022-11-12 14:34:05 -0700

    added comment to project models index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -10,4 +11,21 @@ Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Project.hasMany(Comment, {
+  foreignKey: 'project_id'
+});
+
+Comment.belongsTo(Project, {
+  foreignKey: 'project_id'
+});
+
+module.exports = { User, Project, Comment };
